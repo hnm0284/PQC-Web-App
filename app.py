3670d613042795380f9b0f96fccf98c2df559efc
Hassan Naeem <98180814+hnm0284@users.noreply.github.com>
2025-04-21 22:38:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ from cryptography.hazmat.primitives import padding
 import os
 import base64
 
-
 # --- START OF SECURITY SETUP ---
 
 from flask_talisman import Talisman
 from dotenv import load_dotenv
-import os
 
 # Load environment variables
 load_dotenv()
@@ -26,36 +24,33 @@ Talisman(app)
 
 # --- END OF SECURITY SETUP ---
 
-app = Flask(__name__)
-
 # In-memory storage (temporary)
-stored_keys = {
+storedKeys = {
     "public": None,
     "private": None,
-    "ciphertext_kem": None,
-    "encrypted_message": None
+    "ciphertextKem": None,
+    "encryptedMessage": None
 }
 
 # --- AES Utility Functions ---
 
-def aes_encrypt(message: str, key: bytes) -> bytes:
+def aesEncrypt(message: str, key: bytes) -> bytes:
     iv = os.urandom(16)
-    #iv = b'1234567890abcdef'  # initialization vector
     padder = padding.PKCS7(128).padder()
-    padded_msg = padder.update(message.encode()) + padder.finalize()
+    paddedMsg = padder.update(message.encode()) + padder.finalize()
     cipher = Cipher(algorithms.AES(key[:32]), modes.CBC(iv))
     encryptor = cipher.encryptor()
-    encrypted_msg = encryptor.update(padded_msg) + encryptor.finalize()
-    return iv + encrypted_msg
+    encryptedMsg = encryptor.update(paddedMsg) + encryptor.finalize()
+    return iv + encryptedMsg
 
-def aes_decrypt(ciphertext: bytes, key: bytes) -> str:
+def aesDecrypt(ciphertext: bytes, key: bytes) -> str:
     iv = ciphertext[:16]
-    encrypted_msg = ciphertext[16:]
+    encryptedMsg = ciphertext[16:]
     cipher = Cipher(algorithms.AES(key[:32]), modes.CBC(iv))
     decryptor = cipher.decryptor()
-    padded_msg = decryptor.update(encrypted_msg) + decryptor.finalize()
+    paddedMsg = decryptor.update(encryptedMsg) + decryptor.finalize()
     unpadder = padding.PKCS7(128).unpadder()
-    message = unpadder.update(padded_msg) + unpadder.finalize()
+    message = unpadder.update(paddedMsg) + unpadder.finalize()
     return message.decode()
 
 # --- Flask Routes ---
@@ -65,50 +60,45 @@ def index():
     return render_template('index.html')
 
 @app.route('/generate_keys', methods=['POST'])
-def generate_keys():
-    pub_key, priv_key = ML_KEM_512.keygen()
-    stored_keys['public'] = pub_key
-    stored_keys['private'] = priv_key
+def generateKeys():
+    pubKey, privKey = ML_KEM_512.keygen()
+    storedKeys['public'] = pubKey
+    storedKeys['private'] = privKey
     return jsonify({
-        'public_key': base64.b64encode(pub_key).decode(),
-        'private_key': base64.b64encode(priv_key).decode()
+        'public_key': base64.b64encode(pubKey).decode(),
+        'private_key': base64.b64encode(privKey).decode()
     })
 
 @app.route('/encrypt', methods=['POST'])
-def encrypt_message():
+def encryptMessage():
     message = request.form.get('message')
     
     # Check if the public key exists, if not, generate the keys first
-    pub_key = stored_keys['public']
-    if not pub_key:
-        # Generate keys if not available
-        pub_key, priv_key = ML_KEM_512.keygen()
-        stored_keys['public'] = pub_key
-        stored_keys['private'] = priv_key
+    pubKey = storedKeys['public']
+    if not pubKey:
+        pubKey, privKey = ML_KEM_512.keygen()
+        storedKeys['public'] = pubKey
+        storedKeys['private'] = privKey
         
-    shared_key, ciphertext_kem = ML_KEM_512.encaps(pub_key)
-    encrypted_msg = aes_encrypt(message, shared_key)
-    stored_keys['ciphertext_kem'] = ciphertext_kem
-    stored_keys['encrypted_message'] = encrypted_msg
+    sharedKey, ciphertextKem = ML_KEM_512.encaps(pubKey)
+    encryptedMsg = aesEncrypt(message, sharedKey)
+    storedKeys['ciphertextKem'] = ciphertextKem
+    storedKeys['encryptedMessage'] = encryptedMsg
     return jsonify({
-        'encrypted': base64.b64encode(encrypted_msg).decode()
+        'encrypted': base64.b64encode(encryptedMsg).decode()
     })
 
-
 @app.route('/decrypt', methods=['POST'])
-def decrypt_message():
-    priv_key = stored_keys['private']
-    ciphertext_kem = stored_keys['ciphertext_kem']
-    encrypted_msg = stored_keys['encrypted_message']
-    if not priv_key or not ciphertext_kem or not encrypted_msg:
+def decryptMessage():
+    privKey = storedKeys['private']
+    ciphertextKem = storedKeys['ciphertextKem']
+    encryptedMsg = storedKeys['encryptedMessage']
+    if not privKey or not ciphertextKem or not encryptedMsg:
         return jsonify({'error': 'Missing encryption data'}), 400
-    shared_key = ML_KEM_512.decaps(priv_key, ciphertext_kem)
-    original_msg = aes_decrypt(encrypted_msg, shared_key)
-    return jsonify({'decrypted': original_msg})
-
-import os
+    sharedKey = ML_KEM_512.decaps(privKey, ciphertextKem)
+    originalMsg = aesDecrypt(encryptedMsg, sharedKey)
+    return jsonify({'decrypted': originalMsg})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))  # default to 10000 if not set
     app.run(host='0.0.0.0', port=port)
-
